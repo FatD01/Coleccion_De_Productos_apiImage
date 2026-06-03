6651b693070098aee7e42940f901904abfcc0bae
FatD01 <fatima.rodriguez@tecsup.edu.pe>
2026-06-03 17:53:22 -0500

    fixeado aiven ya reconoce el back

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
+const configPool = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
-});
+    queueLimit: 0,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
+};
+
+
+if (process.env.DB_HOST && process.env.DB_HOST.includes('aivencloud.com')) {
+    configPool.ssl = { rejectUnauthorized: false };
+}
+
+const pool = mysql.createPool(configPool);
 
 module.exports = pool;
